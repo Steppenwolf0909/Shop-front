a3a1b8c9be41a66d373a32211297ce6441485136
Steppenwolf09 <Steppenwolf09@yandex.ru>
2022-08-08 14:32:49 +0300

    init

---
+++ src/Product.tsx
@@ -0,0 +1,27 @@
+import {useState} from "react";
+
+
+export default function Product(props: any){
+  const [details, setDetails] = useState(false)
+  const btnColor = details ? 'bg-yellow-400' : 'bg-blue-400'
+  const btnClass = ['py-2 px-4 border', btnColor]
+  return (
+    <div>
+      <h1>
+      {props.prod.title}
+      </h1>
+      <span>{props.prod.price}</span>
+      <img className="w-1/6" src={props.prod.image} />
+      <button
+        className={btnClass.join(' ')}
+        onClick={() => {setDetails(!details)}}>
+        { details ? 'Show' : 'Hide'}
+      </button>
+
+      {details && <span>
+        {props.prod.description}
+      </span>
+      }
+    </div>
+  )
+}
